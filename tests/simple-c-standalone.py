20bcdf6110af4f7c3acfbcb6f34c56c7afa17090
Ruben A. Tikidji-Hamburyan <rth@pop-os.localdomain>
2022-01-21 20:57:51 -0500

    the same silly test for cpp brantch

---
+++ tests/simple-c-standalone.py
@@ -0,0 +1,62 @@
+from numpy import *
+from brian2 import *
+from dcv4brian import *
+
+
+defaultclock.dt=0.1*ms
+set_device('cpp_standalone', directory='standalone')
+
+taum = 20*ms
+taue = 5*ms
+Vt = -50
+Vr = -60
+El = -55
+
+
+
+eqs = '''
+dv/dt  = (I + int(i==0)*int(t<40*ms)*10 -(v-El))/taum : 1 (unless refractory)
+dI/dt  = (S - I)/taue     : 1 (unless refractory)
+S : 1
+'''
+
+N = NeuronGroup(4, eqs, threshold='v>Vt', reset='v = Vr; I = 0', refractory=1*ms, method='euler')
+N.v = 'Vr'
+
+
+W = Synapses(N, N, """
+    w : 1 # synaptic weight
+    dly : integer (constant) # delay
+    S_post = w/(1+exp(-(dcvget(dly,i)-Vt))) : 1  (summed)
+    #S_post = w/(1+exp(-(v_pre-Vt))) : 1  (summed)
+""")
+
+xdly = array([ int(round((100+i*10)*ms/defaultclock.dt)) for i in range(4) ])
+W.connect(i=[0,1,2,3],j=[1,2,3,0])
+W.dly = xdly
+W.w   = 50.
+
+M = StateMonitor(N, ['v','I','S'], record=True)
+dcvinit(amax(xdly).astype(int)+5,4,array([Vr,Vr,Vr,Vr]),c_target=True)
+updater     = N.run_regularly('return_ = dcvupdate(v, i)')
+
+
+run(2000*ms)
+
+axv = subplot(3,1,1)
+plot(M.t/ms,M[0].v,"-",lw=2,label="0")
+plot(M.t/ms,M[1].v,"-",lw=2,label="1")
+plot(M.t/ms,M[2].v,"-",lw=2,label="2")
+plot(M.t/ms,M[3].v,"-",lw=2,label="3")
+legend(loc=0)
+axi = subplot(3,1,2,sharex=axv)
+plot(M.t/ms,M[0].I,"-")
+plot(M.t/ms,M[1].I,"-")
+plot(M.t/ms,M[2].I,"-")
+plot(M.t/ms,M[3].I,"-")
+axs = subplot(3,1,3,sharex=axv)
+plot(M.t/ms,M[0].S,"-")
+plot(M.t/ms,M[1].S,"-")
+plot(M.t/ms,M[2].S,"-")
+plot(M.t/ms,M[3].S,"-")
+show()
