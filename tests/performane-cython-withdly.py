19c2d69f302d0e58660a01bc5afb4646917eeb2e
Ruben A. Tikidji-Hamburyan <rth@pop-os.localdomain>
2022-01-31 21:30:06 -0500

    Random connections with and without delayed variables

--- tests/performane-cython-withdly.py
+++ tests/performane-cython-withdly.py
@@ -18,26 +18,27 @@ El = -55
 
 eqs = '''
 dv/dt  = (I + int(i==0)*int(t<40*ms)*10 -(v-El))/taum : 1 (unless refractory)
-dI/dt  = (w/(1+exp(-(S-Vt))) - I)/taue     : 1 (unless refractory)
+dI/dt  = (50/(1+exp(-(w*S-Vt))) - I)/taue     : 1 (unless refractory)
 S : 1
 w : 1
 '''
 
 N = NeuronGroup(Nnrn, eqs, threshold='v>Vt', reset='v = Vr; I = 0', refractory=1*ms, method='euler')
 N.v = 'Vr'
-N.w   = 50.
+N.w   = 0.01
 N.S = 'Vr'
 
 
 W = Synapses(N, N, """
-    dly                     : integer (constant) # delay
-    S_post = dcvsetget(i,v_pre,dly,t/ms)  : 1  (summed)
+    dly                                             : integer (constant) # delay
+    S_post = dcvsetget(i,v_pre,dly,t_in_timesteps)  : 1  (summed)
 """)
-xdly  = array([ 200+i*3 for i in range(Nnrn) ])
-W.connect(j='(i+1)%Nnrn')
-W.dly = xdly
+xdly  = array([ [f,t,200+f*3] for f in range(Nnrn) for t in range(Nnrn) if f !=t and random() < 0.3])
 
-dcvinit(amax(xdly).astype(int)+5,Nnrn,defaultclock.dt/ms,array([Vr for l in range(Nnrn)]),c_target=True)
+W.connect(i=xdly[:,0].flatten(), j=xdly[:,1].flatten())
+W.dly = xdly[:,2].flatten()
+
+dcvinit(amax(xdly[:,2]).astype(int)+5,xdly.shape[0],array([Vr for l in xdly]),c_target=True)
 
 
 run(20000*ms,report='text')
--- tests/performane-cython-withoutdly.py
+++ tests/performane-cython-withoutdly.py
@@ -17,24 +17,26 @@ El = -55
 
 eqs = '''
 dv/dt  = (I + int(i==0)*int(t<40*ms)*10 -(v-El))/taum : 1 (unless refractory)
-dI/dt  = (w/(1+exp(-(S-Vt))) - I)/taue     : 1 (unless refractory)
+dI/dt  = (50/(1+exp(-(w*S-Vt))) - I)/taue     : 1 (unless refractory)
 S : 1
 w : 1
 '''
 
 N = NeuronGroup(Nnrn, eqs, threshold='v>Vt', reset='v = Vr; I = 0', refractory=1*ms, method='euler')
 N.v = 'Vr'
-N.w   = 50.
+N.w   = 0.01
 N.S = 'Vr'
 
 
 W = Synapses(N, N, """
-    dly                     : integer (constant) # delay
+    dly                                             : integer (constant) # delay
     S_post = v_pre  : 1  (summed)
 """)
-xdly  = array([ 200+i*3 for i in range(Nnrn) ])
-W.connect(j='(i+1)%Nnrn')
-W.dly = xdly
+xdly  = array([ [f,t,200+f*3] for f in range(Nnrn) for t in range(Nnrn) if f !=t and random() < 0.3])
+
+W.connect(i=xdly[:,0].flatten(), j=xdly[:,1].flatten())
+W.dly = xdly[:,2].flatten()
+
 
 
 
